5fce75709b3f64d982bbb7616dc6653eb82b0b00
Madhumitha2012 <madhumithak762@gmail.com>
2026-09-09 18:52:16 +0530

    Add button-controlled LED functionality

---
+++ Activity-02_Push_Button/code.ino
@@ -0,0 +1,17 @@
+int buttonPin = 8;
+int ledPin = 13;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == LOW) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
